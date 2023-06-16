f460c19de261bd41cda41599bf85880bfd864f50
ash-tise <110732133+ash-tise@users.noreply.github.com>
2023-06-16 13:39:22 -0500

    update

--- ytPlaylistDL.py
+++ ytPlaylistDL.py
@@ -1,5 +1,6 @@
 from yt_dlp import YoutubeDL
-import scrapetube
+import scrapetube, os, re
+os.listdir(os.getcwd())
 
 c = input('Enter Playlist URL Here: ')
 play_id = ''
@@ -14,5 +15,22 @@ links = []
 for video in videos:
     links.append('https://www.youtube.com/watch?v='+video['videoId'])
 
-ydl = YoutubeDL()
-ydl.download(links)
+
+with YoutubeDL() as ydl: 
+    for i, link in enumerate(links):
+        info_dict = ydl.extract_info(link, download=False)
+        video_title = info_dict.get('title', None)
+        video_id = info_dict.get('id', None)
+        search_title = f'{video_title} [{video_id}].mp4'
+
+
+
+    
+        try:
+            ydl.download(link)
+        except:
+            print(f'FAILED TO DOWNLOAD {video_title}')
+        try:
+            os.rename(search_title, f'{i+1} {video_title}')
+        except:
+            print(f'FAILED TO RENAME FILE {search_title}')
