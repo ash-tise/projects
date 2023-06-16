183322560b1c78d97a47100974ac48eba11af1f7
ash-tise <110732133+ash-tise@users.noreply.github.com>
2023-06-16 17:29:57 -0500

    numbering

--- ytChannelDL.py
+++ ytChannelDL.py
@@ -1,5 +1,5 @@
 from yt_dlp import YoutubeDL
-import scrapetube
+import scrapetube, os
 
 c = input('Enter YT Channel Here: ')
 
@@ -8,6 +8,21 @@ links = []
 for video in videos:
     links.append('https://www.youtube.com/watch?v='+video['videoId'])
 
+with YoutubeDL() as ydl: 
+    for i, link in enumerate(links):
+        info_dict = ydl.extract_info(link, download=False)
+        video_title = info_dict.get('title', None)
+        video_id = info_dict.get('id', None)
+        search_title = f'{video_title} [{video_id}].mp4'
 
-ydl = YoutubeDL()
-ydl.download(links)
+
+
+    
+        try:
+            ydl.download(link)
+        except:
+            print(f'FAILED TO DOWNLOAD {video_title}')
+        try:
+            os.rename(search_title, f'{i+1} {video_title}.mp4')
+        except:
+            print(f'FAILED TO RENAME FILE {search_title}')
